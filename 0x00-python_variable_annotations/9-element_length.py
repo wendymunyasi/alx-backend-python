281cce369963704cae8eb62f882eed60b7a65b04
wendy munyasi <wendymunyasi@gmail.com>
2023-01-06 12:49:27 +0300

    Add task 3

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Module for task 9
+"""
+
+from typing import List, Tuple
+
+
+def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+    """Creates a list of tuples, where each tuple contains a string from
+    the given list and its length.
+
+    Args:
+        lst (List[str]): The list of strings to process.
+
+    Returns:
+         List[Tuple[str, int]]: A list of tuples, where each tuple contains
+         a string from the original list and its length.
+    """
+    return [(i, len(i)) for i in lst]
