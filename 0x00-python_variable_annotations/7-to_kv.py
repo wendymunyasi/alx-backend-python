d75f7153fb2a8f0b93b2461ac1c3439fd924dec0
wendy munyasi <wendymunyasi@gmail.com>
2023-01-06 12:50:03 +0300

    Add task 7

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Module for task 7
+"""
+
+
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Type-annotated function that takes a string k and an int OR float v
+    as arguments and returns a tuple.
+
+    Args:
+        k (str): The string to include in the tuple.
+        v (Union[int, float]): The integer or float to square and
+        include in the tuple.
+
+    Returns:
+        Tuple[str, float]: The tuple with the string as the first element
+        and the squared integer or float as the second element.
+    """
+    return (k, float(v**2))
