9168af2742e108d564ff2837cd4bc8c67f9b65c2
wendy munyasi <wendymunyasi@gmail.com>
2023-01-06 12:49:04 +0300

    Add task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Module for task 1
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Type-annotated function that takes a string str1 and a string
+    str2 as arguments and returns a concatenated string.
+
+    Args:
+        str1 (str): first string to concatenate.
+        str2 (str): second string to concatenate.
+
+    Returns:
+        str: The concatenated string.
+    """
+    return str1 + str2
