7ef3119e7a44444b7b535e93bb1f8392d82d6f13
wendy munyasi <wendymunyasi@gmail.com>
2023-01-06 12:49:33 +0300

    Add task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Module for task 3
+"""
+
+
+def to_str(n: float) -> str:
+    """Type-annotated function that takes a float n as argument and returns
+    the string representation of the float.
+
+    Args:
+        n (float): float to convert.
+
+    Returns:
+        str: String representation of the float
+    """
+    return str(n)
