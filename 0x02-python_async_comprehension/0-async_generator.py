e33a124a9150a63c37610510e6a6b6b7dd86bd6e
wendy munyasi <wendymunyasi@gmail.com>
2023-01-11 14:19:53 +0300

    Add task 0 - coroutine called async_generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Module for task 0
+"""
+
+
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator():
+    """Coroutine that generates random numbers between 0 and 10, one number
+    at a time.
+
+    Yields:
+        Generator[float, None, None]:  A random number between 0 and 10.
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.randint(0, 10)
